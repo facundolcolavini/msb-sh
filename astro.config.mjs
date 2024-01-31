abf57023e7ad403aaacc2827ed6fe5e1b9ceb409
facundolcolavini <fcolavini@southernhorizon.tech>
2024-01-31 18:07:00 -0300

    Update vercel import in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 import icon from "astro-icon";
 import { defineConfig, squooshImageService } from "astro/config";
 
