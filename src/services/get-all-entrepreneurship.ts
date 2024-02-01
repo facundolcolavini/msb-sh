14ae4317a3319132087147aba924113292f2d18b
facundolcolavini <fcolavini@southernhorizon.tech>
2024-02-01 13:00:44 -0300

    Fix Google Maps icon URL and add new pages

---
+++ src/services/get-all-entrepreneurship.ts
@@ -0,0 +1,28 @@
+// entrepreneurships.service.ts
+import { fetchData, type AdditionalParams } from '../utils/fetch-data';
+import type { APIResponseEntrepreneurship, Results as EntrepreneurshipResult } from '../interfaces/entrepreneurship.interfaces.ts';
+const endpoint = 'resultados.emprendimientos';
+
+export const getAllEntrepreneurships = async (): Promise<EntrepreneurshipResult> => {
+  try {
+    const res = await fetchData(endpoint) as APIResponseEntrepreneurship;
+    return res.resultado;
+  } catch (error) {
+    console.error('Error fetching entrepreneurship data:', error);
+    throw error;
+  }
+};
+
+// Get by ID 
+export const getEntrepreneurshipById = async (id :string): Promise<EntrepreneurshipResult> => {
+  const queryParams = {
+    ed_idl: id.toString(),
+  };
+  try {
+    const res = await fetchData(endpoint, queryParams) as APIResponseEntrepreneurship;
+    return res.resultado;
+  } catch (error) {
+    console.error('Error fetching entrepreneurship data:', error);
+    throw error;
+  }
+}
--- src/utils/getCode.ts
+++ src/utils/getCode.ts
@@ -1,18 +1,38 @@
 /* Funcion que saca del string el codigo  */
-/* Ejemplo :
-    Input: "lote-en-venta-en-san-antonio-de-padua-ficha-ms2CODIGOAOBTENER"
-    Output: "CODIGOAOBTENER"
+/* Ejemplo : 
+    Input: "lote-en-venta-en-san-antonio-de-padua-ficha-ms2CODIGOAOBTENER" || edificio-tucuman-1000-en-moron-norte-morn-emprendimiento-44 o tambien condominio-soler-en--ituzaing-norte-ituzaing-norte-emprendimiento-63
+    Output: "CODIGOAOBTENER" || 44
 */
 
-export function getCode(str:string) {
-    let code = str.split('-').pop();
-
-    if (code?.includes('ms')) {
-        code = code.split('ms')[1];
-    }
-    // Sacar el primer numero del codigo y devolver el resto 
-    if (code?.match(/^\d/)) {
+export function getCode(str: string): string {
+    // Divide la cadena en segmentos usando guiones ("-")
+    const segments = str.split('-');
+  
+    // Encuentra el último segmento que es un número o contiene 'ms'
+    const lastSegment = segments.reverse().find(segment => segment.match(/^\d/) || segment.includes('ms'));
+  
+    let code = '';
+  
+    if (lastSegment) {
+      // Si el último segmento contiene 'ms', extrae el código después de 'ms'
+      if (lastSegment.includes('ms')) {
+        code = lastSegment.split('ms')[1];
+      } else {
+        code = lastSegment;
+      }
+  
+      // Elimina el primer número del código
+      if (code.match(/^\d/)) {
         code = code.slice(1);
+      }
+  
+      // Elimina el último número del código
+      if (code.match(/\d$/)) {
+        code = code.slice(0, -1);
+      }
     }
+  
+    console.log(code);
     return code;
-} 
+  }
+  
