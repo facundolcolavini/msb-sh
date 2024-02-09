1a652485a2c78862e2a7458aaf4695e2b6a53734
facundolcolavini <fcolavini@southernhorizon.tech>
2024-02-09 13:34:54 -0300

    BreadCrumb skeleton

--- src/components/preact/Property/PropertyPage.tsx
+++ src/components/preact/Property/PropertyPage.tsx
@@ -7,6 +7,7 @@ import GalleryProperty from "../Gallery/GalleryProperty";
 import GalleryPropertySkeleton from "../Skeletons/GalleryPropertySkeleton";
 import Spinner from "../Spinner";
 import Button from "../ui/Buttons/Button";
+import BreadCrumbSkeleton from "../Skeletons/BreadCrumbSkeleton";
 interface Props {
     branchCode: string;
     propertyCode: string;
@@ -48,7 +49,8 @@ const PropertyPage: FunctionComponent<PropsWithChildren<Props>> = (props) => {
     return (
         <>
             <header class="lg:flex justify-between items-center">
-                {!isLoading && props.breadCrumbChild}
+         
+                {isLoading ? <BreadCrumbSkeleton /> : props.breadCrumbChild}
                 <a href={"https://api.whatsapp.com/send/?phone=5491133927629&text=%C2%A1Hola%21+Me+contacto+por+la+siguiente+propiedad%3A+https%3A%2F%2Fmatiasszpira.com.ar%2Fcasa-en-alquiler-en-terravista-ficha-ms36639&type=phone_number&app_absent=0"} class="bg-primary-bg-hover-msb py-3 h-fit rounded-lg px-12 lg:text-lg md:text-md text-white tracking-wide">Consultar</a>
             </header>
             <div class="pt-5 flex justify-between">
