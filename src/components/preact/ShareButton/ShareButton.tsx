bb10c5d794cb3ff970a04a11b02c7cf6bb5a3c85
facundolcolavini <fcolavini@southernhorizon.tech>
2024-02-16 13:21:30 -0300

    Update WhatsApp share link in ShareButton component

--- src/components/preact/ShareButton/ShareButton.tsx
+++ src/components/preact/ShareButton/ShareButton.tsx
@@ -54,7 +54,7 @@ const ShareButton = () => {
       >
         <a
           target="_blank"
-          href={`https://api.whatsapp.com/send?phone=&text=Hola%2C%20te%20comparto%20esta%20ficha%3A%20${he.decode(window.location.href)}&source=&data=`}
+          href={`https://api.whatsapp.com/send?phone=&text=${he.decode(window.location.href)}`}
           className="block p-1 rounded-full hover:bg-gray-200 transition-colors duration-300"
           title="Compartir en WhatsApp"
         >
