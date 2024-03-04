87d7f019cbd0c570d4ec00739eafe1fab7a030f6
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-04 17:29:48 -0300

    Delete unused files and update HeartIcon stroke color

--- src/components/preact/Icons/WhatsAppIcon copy.tsx
+++
@@ -1,9 +0,0 @@
-
-export const WhatsAppIcon = () => {
-  return (
-    <>
-   
-    <img id="bath" alt="whatsapp icon"  className={'object-contain'} src="/images/whatsapp.png" width="24" height="24" />
-    </>
-  )
-}
