7271bc8d017975ad4adda58db30200a16d69ad95
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-13 13:55:28 -0300

    Fix user seeding code

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,18 +1,18 @@
 
 import type { APIRoute } from 'astro';
-import { Users, db } from 'astro:db';
+
 
 // Astro API endpoint for user CRUD 
 
 export const GET: APIRoute = async (ctx) => {
 
 
-            await db.insert(Users).values([
+         /*    await db.insert(Users).values([
                 { id: 5, name: 'John', lastName: 'Doe', password:'', email: ' john@ doe.com' },
                 { id: 6, name: 'Jane', lastName: 'Doe', password:'',email: ' jane@ doe.com' },
             ]);
               await db.select().from(Users);
-    
+     */
                 return new Response(JSON.stringify( 'OK SEED' ), {
                     headers: {
                         'content-type': 'application/json',
