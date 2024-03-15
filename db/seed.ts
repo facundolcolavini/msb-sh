75a2faf4bd18df9e232281ef0559074e446a2a3c
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-15 09:40:16 -0300

    Add API routes for deleting and updating user records

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,10 @@
-import { db, Users } from 'astro:db';
 
 export default async function () {
 
   //Delete all records from the table and reset the auto incrementing primary key 
 
   // Seed your database with users here
-  await db.insert(Users).values([
-    { id: Math.floor(Math.random() * 1000000), name: 'John', lastName: 'Doe', password:'', email: 'john@doe.com' },
-    { id: Math.floor(Math.random() * 1000000), name: 'Jane', lastName: 'Doe', password:'', email: 'jane@doe.com' },
-]);
+/*   await db.query(Users.delete()); */
 
 
 }   
