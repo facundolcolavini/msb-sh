451a96b3cbc958e4088077f887a60a5e44b13c41
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-15 08:36:30 -0300

    Delete deleteUser.ts and [id].json.ts, and update index.astro

--- src/models/users/deleteUser.ts
+++
@@ -1,9 +0,0 @@
-import { eq } from "astro:dbg";
-import { db, Users } from "astro:db";
-
-type DeleteUser = typeof Users.$inferInsert;
-
- export  const  deleteUser =  async(user: DeleteUser) => {
-  return await db.delete().from(Users).where(eq(Users.id, user.id))
-}
-
