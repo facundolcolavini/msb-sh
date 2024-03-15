e3cc6e326118b24ec29749222ec15b22300f95b1
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-15 10:32:33 -0300

    Add GET endpoint and update form functionality

--- src/pages/api/[id].json.ts
+++ src/pages/api/[id].json.ts
@@ -85,3 +85,43 @@ export const PATCH: APIRoute = async ({ params, request }) => {
     );
   }
 };
+
+export const GET: APIRoute = async ({ params }) => {
+    const id = Number(params.id);
+    console.log(id)
+    if (!id) {
+      return new Response(
+        JSON.stringify({
+          message: "Please provide all required fields.",
+          success: false,
+        }),
+        {
+          status: 404,
+        }
+      );
+    }
+  
+    try {
+      const res = await db.select().from(Users).where(eq(Users.id, id));
+      console.log(res)
+      if (res) {
+        return  new Response( JSON.stringify(res), { 
+            headers: { "content-type": "application/json" },
+            });
+      } else {
+        throw new Error("prob, bob");
+      }
+    } catch (e) {
+      console.error(e);
+      return new Response(
+        JSON.stringify({
+          message: e,
+          success: false,
+        }),
+        {
+          status: 404,
+        }
+      );
+    }
+  };
+  
