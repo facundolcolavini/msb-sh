6dc45c6ed46f64b1ab4aecd1b3232b058fad1dab
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-15 08:39:26 -0300

    Delete unused user-related files and fix indentation in usuarios/index.astro

--- src/models/users/insertUser.ts
+++
@@ -1,8 +0,0 @@
-import { db, Users } from "astro:db";
-
-type NewUser = typeof Users.$inferInsert;
-
- export  const  insertUser = async (user: NewUser) => {
-  return await db.insert(Users).values(user);
-}
-
