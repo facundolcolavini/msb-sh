ed28b1f92d9560467ffbb09beb2bdef3d92e1256
Facundo Colavini <fcolavini@southernhorizon.tec>
2024-03-26 09:48:34 -0300

    Add prerender flag and update ResultsPage session

--- src/pages/api/results.json.ts
+++ src/pages/api/results.json.ts
@@ -2,7 +2,7 @@
 import type { APIResponseResultsRecords } from '@/interfaces/results.records.interfaces';
 import { fetchData } from '@/utils/fetch-data';
 import type { APIRoute } from 'astro';
-
+export const  prerender = true;
 
 
 
