0523ae26f1b900fe98644e580dd0a20edbce8655
Facundo Colavini <fcolavini@southernhorizon.tec>
2024-03-26 12:13:21 -0300

    Remove @astrojs/vercel and add @astrojs/netlify as adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
+import netlify from '@astrojs/netlify';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
   integrations: [db(), tailwind(), icon(), preact()],
   output: "server",
   vite: {
