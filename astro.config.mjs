7c167e9414f2a8262431445559bb3408f1ab339a
facundolcolavini <fcolavini@southernhorizon.tech>
2024-03-27 14:32:55 -0300

    Update output to 'hybrid' in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), preact({
     include: ["**/preact/*"]
   }), icon(), db()],
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
   image: {
     service: squooshImageService()
