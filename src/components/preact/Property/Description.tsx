f13fdff6cc66465f071f4c8a0c7d5e29266987b8
facundolcolavini <fcolavini@southernhorizon.tech>
2024-05-10 16:24:07 -0300

    refactor: Add lazy loading for property description in Description.tsx

--- src/components/preact/Property/Description.tsx
+++ src/components/preact/Property/Description.tsx
@@ -1,4 +1,5 @@
 import he from 'he';
+import { useEffect, useRef } from 'preact/hooks';
 
 interface Props {
   htmlText: string;
@@ -7,9 +8,22 @@ interface Props {
 const Description = ({ htmlText }: Props) => {
   // Decodificar el texto HTML
   const decodedText = he.decode(htmlText);
+  // Use userRef to create a div component with the decoded HTML text 
+  const divRef = useRef<HTMLDivElement>(null);
+
+  // load the decoded text into the divRef
+   useEffect(() => {
+    if (divRef.current) {
+      divRef.current.innerHTML = decodedText;
+    }
+  } , [decodedText]);
 
   // Crear un componente div con el texto HTML decodificado
-  return <p className={'text-base'} dangerouslySetInnerHTML={{ __html: decodedText }} />;
+  return (
+    <div className={'relative overflow-clip w-100 text-base'} ref={divRef}>
+     
+      </div>)
+
 };
 
 export default Description;
