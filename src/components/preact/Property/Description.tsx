d93db23c0378eb674099a11511aeccd1ff003dcd
facundolcolavini <fcolavini@southernhorizon.tech>
2024-05-10 16:25:51 -0300

    refactor: Update property description rendering in Description.tsx

--- src/components/preact/Property/Description.tsx
+++ src/components/preact/Property/Description.tsx
@@ -9,20 +9,20 @@ const Description = ({ htmlText }: Props) => {
   // Decodificar el texto HTML
   const decodedText = he.decode(htmlText);
   // Use userRef to create a div component with the decoded HTML text 
-  const divRef = useRef<HTMLDivElement>(null);
+  const pRef = useRef<HTMLParagraphElement>(null);
 
   // load the decoded text into the divRef
    useEffect(() => {
-    if (divRef.current) {
-      divRef.current.innerHTML = decodedText;
+    if (pRef.current) {
+      pRef.current.innerHTML = decodedText;
     }
   } , [decodedText]);
 
   // Crear un componente div con el texto HTML decodificado
   return (
-    <div className={'relative overflow-clip w-100 text-base'} ref={divRef}>
+    <p className={'relative overflow-clip w-100 text-base'} ref={pRef}>
      
-      </div>)
+      </p>)
 
 };
 
