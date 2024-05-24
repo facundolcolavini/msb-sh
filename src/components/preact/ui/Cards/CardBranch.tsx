f632316d0f8da00ada99c44ec6aa48a1cdacf173
facundolcolavini <fcolavini@southernhorizon.tech>
2024-05-24 12:30:06 -0300

    refactor: Improve lazy loading for images in CardBranch component

--- src/components/preact/ui/Cards/CardBranch.tsx
+++ src/components/preact/ui/Cards/CardBranch.tsx
@@ -19,34 +19,34 @@ interface Props extends HTMLAttributes<"div"> {
 }
 
 const CardBranch: FunctionComponent<Props> = ({ href, suc_name, suc_loc, suc_img, suc_dir, suc_phone, whatsAppPhone, addStyles, highlight = false }) => {
-    const styles = twMerge(clsx(" rounded-lg  mx-auto w-full overflow-hidden shadow-lg relative", addStyles));
+    const styles = twMerge(clsx(" rounded-lg h-100 mx-auto w-full overflow-hidden shadow-lg relative", addStyles));
     return (
         <div className={styles}>
             
-            <div class="bg-secondary-msb ">
+            <div class="bg-white ">
             {highlight 
                         && (
                              <p className=" absolute  right-3  top-3 text-md font-bold rounded-full px-3 py-2 bg-secondary-bg-hover-msb hover:bg-bg-1-msb hover:animate-twice text-white   text-sm uppercase">
                         {suc_loc}
                     </p>) }
-                <img className="w-full object-cover" src={suc_img} alt={suc_name} />
-                <div className="p-5 ">
-                    <div className="text-xl font-medium text-nowrap capitalize"><span className="font-extrabold capitalize">{suc_name}</span></div>
-                    <p className="text-bg-2-msb  text-md font-bold">
+                <img className="w-full h-100 object-cover" src={suc_img} alt={suc_name} />
+                <div className="p-5 h-[142px] flex  justify-between flex-col bg-white">
+                    <div className="text-xl font-gothamMedium font-bold text-nowrap capitalize"><span className="font-extrabold capitalize">{suc_name}</span></div>
+                    <p className="text-bg-2-msb  font-gothamMedium font-thin text-md ">
                         {suc_loc}
                     </p>
 
 
-                    <div class="flex justify-between">
-                        <div>
-                            <span className="flex items-center text-sm font-semibold text-gray-700 mr-2 mb-1 ">{suc_dir}</span>
-                            <span className="flex items-center text-xs font-semibold text-gray-700 mr-2 mb-2 gap-1">{suc_phone}</span>
+                    <div class="flex justify-between align-bottom items-end h-100 ">
+                        <div className={''}>
+                            <span className="flex flex-wrap items-center text-xs font-semibold text-gray-700 mr-2 ">{suc_dir}</span>
+                            <span className="flex items-center text-xs font-semibold text-gray-700 gap-1  ">{suc_phone}</span>
                         </div>
 
-                        <div class="flex items-center justify-center gap-1 ">
-                            <a href={href} target="_blank"><PaperLocationIcon className={'h-4 w-4 md:h-5 md:w-5 lg:h-5 lg:w-5 stroke-gray-700'} /></a>
+                        <div class="flex items-center justify-center gap-1 place-content-end self-end ">
+                            <a href={href} target="_blank"><PaperLocationIcon className={'h-5 w-5 md:h-5 md:w-5 lg:h-5 lg:w-5 stroke-gray-700'} /></a>
                             <a href={`https://api.whatsapp.com/send?phone=${whatsAppPhone}`} target="_blank">
-                                <WhatsAppIcon className={'h-4 w-4 md:h-5 md:w-5 lg:h-5 lg:w-5 fill-gray-700'} />
+                                <WhatsAppIcon className={'h-5 w-5 md:h-5 md:w-5 lg:h-5 lg:w-5 fill-gray-700'} />
                             </a>
                         </div>
                     </div>
