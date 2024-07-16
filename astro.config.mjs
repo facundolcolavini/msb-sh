25c298fbddd5f4f8b71c4b83454c1dd3cee49d56
Facundo Colavini <fcolavini@southernhorizon.tec>
2024-07-16 10:20:17 -0300

    refactor: Add @playform/compress for improved lazy loading of images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,30 @@ import vercel from "@astrojs/vercel/serverless";
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import preact from "@astrojs/preact";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [db(), tailwind(), icon(), preact()],
+  integrations: [db(), tailwind(), icon(), preact(), playformCompress(
+    {
+
+        CSS: true,
+        HTML: {
+          'html-minifier-terser': {
+            removeAttributeQuotes: false,
+          },
+        },
+        Image: true,
+        JavaScript: true,
+        SVG: true,
+        Logger: 1,
+    
+    }
+  )],
   vite: {
     optimizeDeps: {
       exclude: ["oslo"]
